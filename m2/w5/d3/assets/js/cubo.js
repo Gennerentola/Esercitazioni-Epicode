5e9768c228470d214aefc1518d462942f9a56ede
Gennerentola <gennaro.notaro1@gmail.com>
2022-10-26 13:09:21 +0200

    implementazione start/stop

--- m2/w5/d3/assets/js/cubo.js
+++ m2/w5/d3/assets/js/cubo.js
@@ -4,6 +4,7 @@ var tastoPlay = document.getElementsByClassName('play-pause');
 var cubo = document.querySelectorAll(".cube")
 var length = cubo.length;
 var trigger = true;
+var interval;
 
 frecciaSx[0].addEventListener('mouseover', function() {
     let angolo = Number(cubo[0].style.transform.replace("rotateY(", "").replace("deg)",""));
@@ -44,5 +45,23 @@ frecciaDx[0].addEventListener('click', function() {
     let angolo = Number(cubo[0].style.transform.replace("rotateY(", "").replace("deg)",""));
     for ( index = 0 ; index < length; index++) {
         cubo[index].style.transform = "rotateY("+(angolo+90)+"deg)";
+    }
+})
+
+function goOn() {
+    let angolo = Number(cubo[0].style.transform.replace("rotateY(", "").replace("deg)",""));
+    for ( index = 0 ; index < length; index++) {
+        cubo[index].style.transform = "rotateY("+(angolo+90)+"deg)";
+    }
+}
+
+tastoPlay[0].addEventListener('click', function() {
+    if (tastoPlay[0].firstChild.className == "fas fa-play") {
+        tastoPlay[0].firstChild.className = "fas fa-stop";
+        interval = setInterval(goOn, 1000);
+        }
+    else {
+        clearInterval(interval);
+        tastoPlay[0].firstChild.className = "fas fa-play";
     }
 })
