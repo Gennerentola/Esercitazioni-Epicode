dc2bdd455fd9202b6d9a352ff12c05cd6393a5a8
Gennerentola <gennaro.notaro1@gmail.com>
2022-11-09 09:04:15 +0100

    ampliamento esercizio OOP II

--- m2/w7/d2/assets/js/paginazione.js
+++ m2/w7/d2/assets/js/paginazione.js
@@ -1,7 +1,7 @@
 var printBox = document.getElementById("itemBox");
 var btnZone = document.getElementById("btnBox");
 
-class Paginate {
+class Pagination {
 
     constructor(_items = [], _pageSize = 10) {
 
@@ -13,10 +13,49 @@ class Paginate {
 
     }
 
-    format() {
-        
+    next() {
 
+        if (this.page < this.numOfPages) {
+        this.currentPage++;
+        }
 
+        this.getView();
     }
 
+    prev() {
+
+        if (this.page > 1) {
+        this.currentPage--;
+        } else {
+            this.currentPage = 1;
+        }
+        this.getView();
+    }
+
+    getView() {
+        let startingIndex = this.pageSize * (this.currentPage-1);
+        let endingIndexNotIncluded = startingIndex + this.pageSize;
+
+        if (endingIndexNotIncluded >= this.items.length) {
+            endingIndexNotIncluded = this.items.length;
+        }
+
+        for (let i = startingIndex; i<endingIndexNotIncluded; i++) {
+            this.addRow(this.items[i])
+        }
+    }
+
+    addRow() {
+        let tr = document.createElement("tr");
+        let tdNum = document.createElement("td");
+        tdNum.innerHTML = element.num;
+        let tdName = document.createElement("td");
+        tdName.innerHTML = element.name;
+        let tdSurname = document.createElement("td");
+        tdSurname.innerHTML = element.surname;
+
+        tr.append(tdNum, tdName, tdSurname);
+
+        printBox.appendChild(tr);
+    }
 }
