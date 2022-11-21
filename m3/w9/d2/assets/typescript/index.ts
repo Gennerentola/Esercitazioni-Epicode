392bec7ed7e41df3e220664bc6f502279a67d327
Gennerentola <gennaro.notaro1@gmail.com>
2022-11-21 11:02:44 +0100

    esercizio d1 e d2 typescript

---
+++ m3/w9/d2/assets/typescript/index.ts
@@ -0,0 +1,51 @@
+class SonAccount {
+    balanceInit: number;
+    constructor (_balanceInit:number = 0) {
+        this.balanceInit = _balanceInit;
+    }
+    oneDeposit() {
+        let partial: number = this.balanceInit + 100;
+        this.oneWithDraw(partial);
+    }
+    oneWithDraw(oldPartial:number) {
+        let partial: number = oldPartial - 25;
+        this.twoDeposit(partial);
+    }
+    twoDeposit(oldPartial:number) {
+        let partial: number = oldPartial +50;
+        this.twoWithDraw(partial);
+    }
+    twoWithDraw(oldPartial:number) {
+        let partial: number = oldPartial - 30;
+        console.log(partial);
+    }
+}
+
+class MotherAccount extends SonAccount {
+    addInterest: number;
+    constructor (_balanceInit: number = 0, _addInterest:number = 0.9) {
+        super(_balanceInit);
+        this.addInterest = _addInterest;
+    }
+    oneDeposit() {
+        let partial: number = this.balanceInit + 200;
+        this.oneWithDraw(partial);
+    }
+    oneWithDraw(oldPartial:number) {
+        let partial: number = oldPartial - 55;
+        this.twoDeposit(partial);
+    }
+    twoDeposit(oldPartial:number) {
+        let partial: number = oldPartial +120;
+        this.twoWithDraw(partial);
+    }
+    twoWithDraw(oldPartial:number) {
+        let partial: number = oldPartial - 65;
+        console.log(partial*this.addInterest)
+    }
+}
+
+var son = new SonAccount();
+son.oneDeposit();
+var mom = new MotherAccount();
+mom.oneDeposit();
